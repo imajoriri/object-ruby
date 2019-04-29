29426338cfaa504798e31a3938c2f4628c0b9191
imajoriri <imajo.srroni@gmail.com>
2019-04-29 14:09:51 +0900

    update

---
+++ ri.rb
@@ -0,0 +1,50 @@
+
+# --- qwan.rb ---
+
+# rakeタスクのリファクタリング
+# serviceについて知ってるのはOK
+
+sheet_names = []
+sheets = []
+
+# シートオブジェクトを保存
+sheet_names.each do |s_name|
+  sheet = Sheet.new(name: s_name, sheet_id: sheet_id)
+  sheet.get_data
+  sheets << sheet
+end 
+
+# それぞれのモデルに保存
+sheets.each do |sheet|
+  sheet.save_data
+end 
+
+# --- Sheet.rb ---
+class Sheet
+  def initialize(args)
+    @name = argsi[:name]
+    @sheet_id = args[:sheet_id]
+    @model = # @nameからモデルを取得
+  end 
+
+  # シートのデータを取得
+  def get_data
+    data = # ... データ取得の処理 ...
+    @data = data
+  end 
+
+  def save_data
+    # Sheetクラスはモデルの情報を知っていてはいけない。メソッドが存在することだけ知っている。
+    # なぜならスプレッドシートの仕様の方が変わりにくいから
+    @model.create_or_update_data(self)
+  end 
+end 
+
+# --- Model.rb ---
+class Model
+  # Modelは割とSheetの情報を知っていてもいい。かわらないから。
+  
+  def self.create_or_update_data(sheet)
+    # ... 色々保存の処理
+  end 
+end 
