d270fa84d181bc6db054e8388f982b3bf7319d04
imajoriri <imajo.srroni@gmail.com>
2019-05-06 12:52:32 +0900

    とりあえず

--- ri.rb
+++ ri.rb
@@ -3,20 +3,22 @@
 
 # rakeタスクのリファクタリング
 # serviceについて知ってるのはOK
+task import_masters do 
 
-sheet_names = []
-sheets = []
+  sheet_names = []
+  sheets = []
 
-# シートオブジェクトを保存
-sheet_names.each do |s_name|
-  sheet = Sheet.new(name: s_name, sheet_id: sheet_id)
-  sheet.get_data
-  sheets << sheet
-end 
+  # シートオブジェクトを保存
+  sheet_names.each do |s_name|
+    sheet = Sheet.new(name: s_name, sheet_id: sheet_id)
+    sheet.get_data
+    sheets << sheet
+  end 
 
-# それぞれのモデルに保存
-sheets.each do |sheet|
-  sheet.save_data
+  # それぞれのモデルに保存
+  sheets.each do |sheet|
+    sheet.save_data
+  end 
 end 
 
 # --- Sheet.rb ---
@@ -30,20 +32,18 @@ class Sheet
   # シートのデータを取得
   def get_data
     data = # ... データ取得の処理 ...
-    @data = data
+      @data = data
   end 
 
   def save_data
     # Sheetクラスはモデルの情報を知っていてはいけない。メソッドが存在することだけ知っている。
     # なぜならスプレッドシートの仕様の方が変わりにくいから
     @model.create_or_update_data(self)
-  end 
-end 
-
-# --- Model.rb ---
-class Model
+ 
+en  --- Model.rb ---
+c ass Model
   # Modelは割とSheetの情報を知っていてもいい。かわらないから。
-  
+
   def self.create_or_update_data(sheet)
     # ... 色々保存の処理
   end 
